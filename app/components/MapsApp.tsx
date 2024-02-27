c5abe9a4cf1495fb8bfa4bf14e97cc6552ffd801
curio13 <manishpndy527@gmail.com>
2024-02-27 13:24:36 +0530

    this  is my last commit

--- app/components/MapsApp.tsx
+++ app/components/MapsApp.tsx
@@ -3,6 +3,9 @@ import { MapContainer,TileLayer,Marker,Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import { Icon } from "leaflet";
 import eventsData from "../historyEvents";
+import { useState } from "react";
+import FlyToMarker from "./FlyToMarker";
+import Filter from "./Filter";
 export interface HistoricalEvent {
     id:number,
     title: String,
@@ -13,7 +16,16 @@ export interface HistoricalEvent {
 
 const defaultPosition: [number,number] = [51.505,-0.09];
 
+const emptyStar = <i className="fa-regular fa-star"></i>;
+const fullStar = <i className="fa-solid fa-star" 
+ style={{
+    color:"#fdc401"
+ }}
+></i>;
+
 function MapsApp() {
+const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
     const icon: Icon = new Icon({
         iconUrl: "marker.svg",
         iconSize:[25,41],
@@ -29,40 +41,114 @@ function MapsApp() {
     //         category:"War"
     //     }
     // ];
+
+    const [activeEvent,setActiveEvent] = useState<HistoricalEvent|null>(null);
+
+    const [favourites,setFavourites] = useState<number[]>(()=>{
+        const savedFavourites = localStorage.getItem("favourites");
+
+        return savedFavourites ? JSON.parse(savedFavourites) :[];
+    })
+
+    const handleFavouriteClick = (eventId: number) =>{
+        let updatedFavourites = favourites.filter((id)=> id!==eventId);
+
+        if(!favourites.includes(eventId))
+        {
+            updatedFavourites = [eventId,...updatedFavourites];
+        }
+
+        setFavourites(updatedFavourites);
+        localStorage.setItem("favourites",JSON.stringify(updatedFavourites));
+    }
+
+    const handleListItemClick = (eventId: number) =>{
+        const event = eventsData.find((event) => event.id === eventId);
+
+        if(event){
+            setActiveEvent(event);
+        }
+    }
+
+
   return (
     <div className="content">
-      <div className="h-12" ></div>
-      <div className="flex flex-col w-full h-full" >
+      <div className="map-content flex flex-col gap-6 h-full" >
+        <Filter setSelectedCategory={setSelectedCategory}/>
       <MapContainer center={defaultPosition} zoom={13}
       className="map-container"
       >
-        <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png/" />
    
-    {
-        eventsData.map((event)=>{
-            return (
-                <Marker key={event.id} position={event.position} icon={icon} >
-                    <Popup>
+        {eventsData
+            .filter(
+              (event) =>
+                !selectedCategory || event.category === selectedCategory
+            )
+            .map((event) => {
+              return (
+                <Marker
+                  key={event.id}
+                  position={event.position}
+                  icon={icon}
+                  eventHandlers={{
+                    click: () => {
+                      setActiveEvent(event);
+                    },
+                  }}
+                />
+              );
+            })}
+     {
+        activeEvent && (
+            <Popup position={activeEvent.position}>
                         <div className="popup-inner">
                             <h2 className="popup-inner__title --red">
-                                {event.title}
+                                {activeEvent.title}
                             </h2>
                         </div>
                         <p className="popup-inner__description" >
-                            {event.description}
+                            {activeEvent.description}
                         </p>
-                        <button>
-                            Favourite
+                        <button  className="popup-inner__button"
+                        onClick={()=> handleFavouriteClick(activeEvent.id)}
+                        >
+                            {favourites.includes(activeEvent.id)?( <span>{fullStar} Unfavourite</span>) : (<span>{emptyStar} favourite</span>)}
                         </button>
                     </Popup>
-                </Marker>
-            )
-        })
+        )
+        }
+    {
+        activeEvent &&(
+            <FlyToMarker position={activeEvent.position} zoomLevel={10} />
+        )
     }
-    
   </MapContainer>
+      </div>
+
+    <div className="liked-events">
+        <h2 className="liked-events__title">
+          <i className="fa-solid fa-star"></i> Favourite Events
+        </h2>
+        <ul>
+          {favourites
+            .map((id) => {
+              return eventsData.find((event) => event.id === id);
+            })
+            .map((event) => {
+              return (
+                <li
+                  key={event?.id}
+                  className="liked-events__event"
+                  onClick={() => {
+                    handleListItemClick(event?.id as number);
+                  }}
+                >
+                  <h3>{event?.title}</h3>
+                </li>
+              );
+            })}
+        </ul>
       </div>
     </div>
   )
